273357110f5f5e4799d0157389b3c3320146e7bf
Radamanthus Batnag <radamanthus@gmail.com>
2016-12-22 14:08:43 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hellorails40::Application.config.secret_key_base = 'b95459dc4db473da4ebd0460569d2338b0f423b29e289fe7c11bbf14a8d90219272d3e78ac9ee249567dbba21a9e95a412a8faf677ac66884fdf5c47f3445ce2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hellorails40::Application.config.session_store :cookie_store, key: '_hellorails40_session'
